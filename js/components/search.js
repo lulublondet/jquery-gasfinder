ce30e286af0b5ff92b8de606c693ccaac798924b
lulublondet <maria.mlvp@gmail.com>
2017-06-07 11:54:25 -0500

    Notacion es6 e identacion

--- js/components/search.js
+++ js/components/search.js
@@ -1,5 +1,5 @@
 'use strict';
-var reRender = function(resultado){
+const reRender =(resultado)=>{
 
 if(resultado.length == 0){
 	alert("Lo sentimos, No hay grifos en ese distrito!!");
@@ -7,17 +7,19 @@ if(resultado.length == 0){
 }
 
 else {
-		var lat = resultado[0].lat;
-		var long = resultado[0].long;
+		const lat = resultado[0].lat;
+		const long = resultado[0].long;
 		
-		var $container = $('<div class="container"></div>');
-		var $row = $('<div class="row"></div>');
-		var $col = $('<div class="col-xs-12 col-md-12"></div>');
-		var $a = $('<a href="mapa.html" class="glyphicon glyphicon-export"></a>');	
-		var $h2 = $('<h2 class="nombreEstacion">'+resultado[0].name+'</h2>');
-		var $p = $('<p>'+resultado[0].address+'</p>');
-		var $span = $('<span>'+resultado[0].district+'</span>');	
-		var $mapa = $('<div id="map"></div')
+		const $container = $('<div class="container"></div>');
+		const $row = $('<div class="row"></div>');
+		const $col = $('<div class="col-xs-12 col-md-12"></div>');
+		const $a = $('<a href="mapa.html" class="glyphicon glyphicon-export"></a>');	
+		const $h2 = $('<h2 class="nombreEstacion">'+resultado[0].name+'</h2>');
+		const $p = $('<p>'+resultado[0].address+'</p>');
+		const $span = $('<span>'+resultado[0].district+'</span>');	
+		const $mapa = $('<div id="map"></div');
+	
+
 			$container.append($row);
 			$row.append($col);
 			$col
@@ -25,43 +27,75 @@ else {
 				.append($h2)
 				.append($p)
 				.append($span);
+
 			$('.detalle').children().hide('slow');		
 			$('.detalle').first().show('slow');	
-			$('.detalle').prepend($container);
+			$('.detalle').prepend($container);	
+
 			$col.append($mapa);	
-			$mapa.insertAfter( $( ".detalle" ) );
-
-		var map = new GMaps({
-    	 div: $mapa[0],
-    	 lat: lat,
-    	 lng: long
-	});
+			$mapa.insertAfter( $(".detalle") );
 
 
-	$a.on("click",function(e){
-	 e.preventDefault();
+/*Funciones pra el mapa */
+/*Ojo-->No debe estar aqui, estoy trabajando para llevarlo al componente corresponidente */
 
-})	;
+	var map = new GMaps({
+    	 	div: $mapa[0],
+    	 	lat: lat,
+    	 	lng: long
+		});
 
-return $('.detalle'); 
+	map.addMarker({
+    		lat: lat,
+    		lng: long,
+    		zoom: 18
+  		});
 
 
+	GMaps.geolocate({
+  		success: function(position) {
+    		map.setCenter(position.coords.latitude, position.coords.longitude);
+  			map.addMarker({
+        		 lat: position.coords.latitude,
+        		 lng: position.coords.longitude,
+        		zoom:18     
+			});
+
+			map.drawRoute({
+        	origin: [position.coords.latitude, position.coords.longitude],
+        	destination: [lat,long],
+        	travelMode: 'driving',
+        	strokeColor: '#aaa',
+        	strokeOpacity: 1,
+        	strokeWeight: 6
+      		});  
+		},
+		error: function(error) {
+    	 alert('No lo pudimos ubicar: '+error.message);
+  		},
+
+  		not_supported: function() {
+    		alert("Actualice el navegador");
+  		},
+  });
+
+	$a.on("click",(e)=>{
+		 e.preventDefault();
+		});
+
+	return $('.detalle'); 
+	}
 }
 
 
-}
-
-
-
-
-var Search = function() {
+var Search = ()=> {
 		
-	var $form = $('<form class="search"></form>');	
-	var $divformGroup = $('<div class="form-group"></div>');
-	var $divInputGroup = $('<div class="input-group"</div>');
-	var $divInputGroupAddon = $('<div class="input-group-addon"></div>');
-	var $spanIcon = $('<span class="glyphicon glyphicon-search"></span>');
-	var $input = $('<input type="text" class="form-control" id="buscarDistrito" placeholder="Ingrese tu distrito a buscar"></input>')
+	const $form = $('<form class="search"></form>');	
+	const $divformGroup = $('<div class="form-group"></div>');
+	const $divInputGroup = $('<div class="input-group"</div>');
+	const $divInputGroupAddon = $('<div class="input-group-addon"></div>');
+	const $spanIcon = $('<span class="glyphicon glyphicon-search"></span>');
+	const $input = $('<input type="text" class="form-control" id="buscarDistrito" placeholder="Ingrese tu distrito a buscar"></input>')
 
 	$form.append($divformGroup);
 	$divformGroup.append($divInputGroup);
@@ -72,9 +106,9 @@ var Search = function() {
 	
 
 	
-	$form.on('submit',function(e){
+	$form.on('submit',(e)=>{
 		e.preventDefault();
-		var resultado =	filterByDistrict(state.stations,$input.val())
+		const resultado =	filterByDistrict(state.stations,$input.val())
 			$input.val("");
 			reRender(resultado);
 			
